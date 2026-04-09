b082b95b8e6fea738acd6dbc5a81508190130e24
MenkeTechnologies <linux.dev25@gmail.com>
2026-04-08 22:24:05 -0400

    scale down benchmarks to realistic sizes for tree-walking interpreter
    Reduce hash from 10k to 1k, regex from global match on 44k string to
    1k iterations of single match, map/grep from 10k to 5k. Previous sizes
    caused multi-minute hangs in benchmarks.

--- bench/bench_pmap_perl.pl
+++ bench/bench_pmap_perl.pl
@@ -1,3 +1,3 @@
-my @data = (1..10000);
+my @data = (1..5000);
 my @result = map { $_ * $_ + $_ * 3 + 7 } @data;
 print scalar @result, "\n";
--- bench/bench_regex.pl
+++ bench/bench_regex.pl
@@ -1,6 +1,8 @@
-my $text = "The quick brown fox jumps over the lazy dog " x 1000;
+my $text = "The quick brown fox jumps over the lazy dog";
 my $count = 0;
-while ($text =~ /\b\w{4,}\b/g) {
-    $count = $count + 1;
+for (my $i = 0; $i < 1000; $i = $i + 1) {
+    if ($text =~ /(\w+)\s+(\w+)$/) {
+        $count = $count + 1;
+    }
 }
 print $count, "\n";
